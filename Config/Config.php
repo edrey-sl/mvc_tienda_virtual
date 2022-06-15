0aee2de144d392caebd046de3baa66cd8d2c0a33
edrey-sl <martinsalinasluna@gmail.com>
2022-06-14 21:24:11 -0500

    tienda virtual

---
+++ Config/Config.php
@@ -0,0 +1,105 @@
+<?php 
+	const BASE_URL = "http://localhost:82/full_tienda";
+	//const BASE_URL = "https://abelosh.com/tiendavirtual";
+
+	//Zona horaria
+	date_default_timezone_set('America/Mexico_City');
+
+	//Datos de conexión a Base de Datos
+	const DB_HOST = "localhost";
+	const DB_NAME = "db_tiendavirtual";
+	const DB_USER = "root";
+	const DB_PASSWORD = "";
+	const DB_CHARSET = "utf8";
+
+	//Para envío de correo
+	const ENVIRONMENT = 1; // Local: 0, Produccón: 1;
+
+	//Deliminadores decimal y millar Ej. 24,1989.00
+	const SPD = ".";
+	const SPM = ",";
+
+	//Simbolo de moneda
+	const SMONEY = "$";
+	const CURRENCY = "MXN";
+
+	//Api PayPal
+	//SANDBOX PAYPAL
+	const URLPAYPAL = "https://api-m.sandbox.paypal.com";
+	const IDCLIENTE = "AW0KEB6ZSA9kEhHAV6cXCIG3AUTbVamAvTim4Co9uu9PY1qZ7duW1CvKasHgmfa0Jys2cWYjUQ1zZKTy";
+	const SECRET = "EOU9CNAY66Vyznf3oqU09DdW7U3ORvJf5HI8aqdD039RujXUURkrhtNWTw0eRAa1Hje2LF82a13N4T-s";
+	//LIVE PAYPAL
+	//const URLPAYPAL = "https://api-m.paypal.com";
+	//const IDCLIENTE = "";
+	//const SECRET = "";
+
+	//Datos envio de correo
+	const NOMBRE_REMITENTE = "Tienda Virtual";
+	const EMAIL_REMITENTE = "tienda@edututos.com";
+	const NOMBRE_EMPESA = "Tienda Virtual";
+	const WEB_EMPRESA = "www.edututos.com";
+
+	const DESCRIPCION = "La mejor tienda en línea con artículos de oficina.";
+	const SHAREDHASH = "TiendaVirtual";
+
+	//Datos Empresa
+	const DIRECCION = "Carretera Cayaco - Puerto Marqués, Del P.r.i. s/n, Acapulco de Juárez, Gro.";
+	const TELEMPRESA = "+(502)78787845";
+	const WHATSAPP = "+524421874969";
+	const EMAIL_EMPRESA = "tienda@edututos.com";
+	const EMAIL_PEDIDOS = "tienda@edututos.com"; 
+	const EMAIL_SUSCRIPCION = "tienda@edututos.com";
+	const EMAIL_CONTACTO = "tienda@edututos.com";
+
+	const CAT_SLIDER = "1,2,3";
+	const CAT_BANNER = "4,5,6";
+	const CAT_FOOTER = "1,2,3,4,5";
+
+	//Datos para Encriptar / Desencriptar
+	const KEY = 'abelosh';
+	const METHODENCRIPT = "AES-128-ECB";
+
+	//Envío
+	const COSTOENVIO = 195;
+
+	//Módulos
+	const MDASHBOARD = 1;
+	const MUSUARIOS = 2;
+	const MCLIENTES = 3;
+	const MPRODUCTOS = 4;
+	const MPEDIDOS = 5;
+	const MCATEGORIAS = 6;
+	const MSUSCRIPTORES = 7;
+	const MDCONTACTOS = 8;
+	const MDPAGINAS = 9;
+
+	//Páginas
+	const PINICIO = 1;
+	const PTIENDA = 2;
+	const PCARRITO = 3;
+	const PNOSOTROS = 4;
+	const PCONTACTO = 5;
+	const PPREGUNTAS = 6;
+	const PTERMINOS = 7;
+	const PSUCURSALES = 8;
+	const PERROR = 9;
+
+	//Roles
+	const RADMINISTRADOR = 1;
+	const RSUPERVISOR = 2;
+	const RCLIENTES = 3;
+
+	const STATUS = array('Completo','Aprobado','Cancelado','Reembolsado','Pendiente','Entregado');
+
+	//Productos por página
+	const CANTPORDHOME = 8;
+	const PROPORPAGINA = 4;
+	const PROCATEGORIA = 4;
+	const PROBUSCAR = 4;
+
+	//REDES SOCIALES
+	const FACEBOOK = "https://www.facebook.com/";
+	const INSTAGRAM = "https://www.instagram.com/";
+	
+
+ ?>
